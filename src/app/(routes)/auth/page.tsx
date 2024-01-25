9d90092a179d72bdd7cf076c98b45523c01ea0fb
Jayson Ng <jaysonng3@gmail.com>
2024-01-25 10:38:27 +0800

    chore: init ui lib and routes

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function LoginPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home</div>
+      <div>Login Page</div>
     </main>
-  )
+  );
 }

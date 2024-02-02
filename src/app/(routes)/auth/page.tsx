e7ab57f4b292486ffe81d350434473e0d7f8509e
Jayson Ng <jaysonng3@gmail.com>
2024-02-03 03:29:55 +0800

    feat: added UserType on registration

--- src/app/(routes)/auth/page.tsx
+++ src/app/(routes)/auth/page.tsx
@@ -1,18 +1,8 @@
 import AuthForm from "@/components/auth/AuthForm"
-import Image from "next/image"
 
 export default async function Auth() {
   return (
     <div className="h-full w-full flex flex-col place-content-center place-items-center bg-gray-100">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          height="48"
-          width="48"
-          className="mx-auto"
-          src=""
-          alt="Pawnection"
-        />
-      </div>
       <AuthForm />
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home</div>
-    </main>
-  )
+  return <div>Home</div>
 }

e7fce1e2a8758c2080722b8512e3025d15657bb7
dylanpeh <e0958646@u.nus.edu>
2024-02-22 14:51:21 +0800

    report missing pets first draft -- can successfully submit, working on handling next

--- src/app/lostAndFound/page.tsx
+++ src/app/lostAndFound/page.tsx
@@ -3,6 +3,5 @@ import LostAndFound from "@/components/LostAndFound/LostAndFound"
 export default async function lostAndFound() {
     return (
         <LostAndFound/>
-
     )
   }
---
+++ src/app/lostPetReport/page.tsx
@@ -0,0 +1,7 @@
+import LostPetReport from "@/components/LostAndFound/LostPetReport"
+
+export default async function lostPetReport() {
+    return (
+        <LostPetReport/>
+    )
+  }

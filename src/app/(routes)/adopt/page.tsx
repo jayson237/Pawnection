9a7723f9436488559e4b526fbdac030a14af13d8
Jayson Ng <jaysonng3@gmail.com>
2024-02-23 23:50:51 +0800

    feat: added cloudinary api

--- src/app/(routes)/adopt/page.tsx
+++ src/app/(routes)/adopt/page.tsx
@@ -1,7 +1,7 @@
-export default async function Home() {
+export default async function Adopt() {
   return (
     <div className="h-full w-full flex flex-col place-content-center place-items-center bg-gray-100">
-      Home
+      Adopt a pet
     </div>
   )
 }

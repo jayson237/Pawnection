02124b907bb81bb1044e56def3a8e2ef873fefff
raymondtju <raymond.rtju@gmail.com>
2024-03-11 16:14:44 +0700

    feat: complete pet lover navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
   return (
-    <div className="h-full w-full flex flex-col place-content-center place-items-center bg-gray-100">
+    <div className="h-full w-full flex flex-col place-content-center place-items-center">
       Home
     </div>
   )

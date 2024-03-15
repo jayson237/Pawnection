f422da56065a77448ecb179067037b305135c4bc
dylanpeh <e0958646@u.nus.edu>
2024-03-15 23:32:39 +0800

    improve UI for dialogs

--- src/components/lostAndFound/FoundPetReportDialog.tsx
+++ src/components/lostAndFound/FoundPetReportDialog.tsx
@@ -13,39 +13,42 @@ import {   Dialog,
     DialogFooter,
     DialogTitle,
     DialogDescription, } from "../ui/Dialog";
-import { Calendar } from "../ui/Calendar";
-import {   DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuGroup,
-  DropdownMenuPortal,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup, } from "../ui/DropdownMenu";
 
-interface LostPetReportDialogProps {
+import { Calendar } from "../ui/Calendar"
+import {   DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuGroup,
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuRadioGroup, } from "../ui/DropdownMenu";
+
+interface FoundPetReportDialogProps {
     isOpen: boolean;
     onClose: () => void; // Assuming onClose does not take any arguments and doesn't return anything
   }
 
-const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
+const FoundPetReportDialog = ({ isOpen, onClose }: FoundPetReportDialogProps) => {
   const [animalType, setAnimalType] = useState("");  
   const [name, setName] = useState("");
   const [sex, setSex] = useState("");
   const [message, setMessage] = useState("");
   const [description, setDescription] = useState("");
-  const [lastSeenArea, setLastSeenArea] = useState("");
-  const [lastSeenDate, setLastSeenDate] = useState<Date | undefined>();
+  const [foundArea, setFoundArea] = useState("");
+  const [foundDate, setFoundDate] = useState<Date | undefined>();
   const [contactDetails, setContactDetails] = useState("");
   const [petImage, setPetImage] = useState<File | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -70,15 +73,15 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
         })
         const resultImage = await cdnResponse.json()
 
-        const response = await fetch("/api/lostAndFound/createLostPetReport", {
+        const response = await fetch("/api/lostAndFound/createFoundPetReport", {
           method: "POST",
           body: JSON.stringify({
             name: name,
             sex: sex,
             message: message,
             description: description,
-            lastSeenArea: lastSeenArea,
-            lastSeenDate: lastSeenDate,
+            foundArea: foundArea,
+            foundDate: foundDate,
             contactDetails: contactDetails,
             petImage: resultImage.secure_url,
           }),
@@ -86,7 +89,7 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
 
         if (!response.ok) {
           throw new Error(
-            "Failed to create Missing Pet Report. Please try again.",
+            "Failed to create Found Pet Report. Please try again.",
           )
         } else {
           setIsLoading(false)
@@ -97,6 +100,8 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
     }
   }
 
+
+
   if (!isOpen) return null;
 
   return (
@@ -104,11 +109,11 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
       <DialogPortal>
         <DialogOverlay className="DialogOverlay" />
         <DialogContent className="DialogContent" style={{ overflowY: 'auto' }}>
-          <DialogTitle className="DialogTitle">Report Missing Pet</DialogTitle>
+          <DialogTitle className="DialogTitle">Report Found Pet</DialogTitle>
 
 
         <form onSubmit={onSubmit}>
-
+          
         <div className="mb-5">
         <DropdownMenu>
             <DropdownMenuTrigger className="border-2 border-black rounded-md">Animal Type: {animalType}</DropdownMenuTrigger>
@@ -120,12 +125,12 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
             </DropdownMenuContent>
         </DropdownMenu>
 
-        </div>            
-        <div className=" mb-5">
+        </div>  
+
+          <div className=" mb-5">
           <div> Name</div>
           <Textarea
             name="petName"
-            required={true}
             onChange={(e) => setName(e.currentTarget.value)}
           />
           </div>
@@ -134,13 +139,12 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
           <div> Gender </div>
           <Textarea
             name="petSex"
-            required={true}
             onChange={(e) => setSex(e.currentTarget.value)}
           />
           </div>
 
           <div className=" mb-5">
-          <div> Message From Owner </div>
+          <div> Message From Founder </div>
           <Textarea
             name="reportMessage"
             required={true}
@@ -158,32 +162,34 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
         </div>
 
         <div className=" mb-5">
-          <div> Area Last Seen </div>
+          <div> Area Found </div>
           <Textarea
-            name="lastSeenArea"
+            name="foundArea"
             required={true}
-            onChange={(e) => setLastSeenArea(e.currentTarget.value)}
+            onChange={(e) => setFoundArea(e.currentTarget.value)}
           />
           </div>
 
-          <div className=" mb-5"> 
-            <div> Last Seen Date </div>
+          <div className=" mb-5">
+          <div> Found Date </div>
+          {/* <Calendar onDayClick={(e) => setFoundDate(e)}  selected={date}/> */}
             <Calendar
-                  mode="single"
-                  selected={lastSeenDate}
-                  onSelect={setLastSeenDate}
-                  className="rounded-md border shadow flex justify-center "
-              />
-            </div>
+                mode="single"
+                selected={foundDate}
+                onSelect={setFoundDate}
+                className="rounded-md border shadow flex justify-center"
+            />
 
-        <div className=" mb-5"> 
+        </div>
+
+        <div className=" mb-5">
           <div> Contact Details </div>
           <Textarea
             name="contactDetails"
             required={true}
             onChange={(e) => setContactDetails(e.currentTarget.value)}
           />
-        </div>
+          </div>
 
           <input
             // className="mr-8 ml-20 w-1/4"
@@ -194,7 +200,7 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
               setPetImage(e.target.files ? e.target.files[0] : null)
             }
           />
-        {/* </div> */}
+        
           
           <div className="flex justify-center">
             <Button className="w-1/6 mt-10 justify-center" type="submit">
@@ -209,4 +215,4 @@ const LostPetReportDialog = ({ isOpen, onClose }: LostPetReportDialogProps) => {
   );
 };
 
-export default LostPetReportDialog;
+export default FoundPetReportDialog;

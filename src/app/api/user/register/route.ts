d5b85b5d7344df01142e147aff719383811593ab
raymondtju <raymond.rtju@gmail.com>
2024-03-17 17:42:58 +0700

    refactor: linting back

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -1,29 +1,29 @@
-import prisma from "@/lib/prismadb"
-import bcrypt from "bcrypt"
-import { NextResponse } from "next/server"
+import prisma from "@/lib/prismadb";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  try {
-    const body = await request.json()
-    const { email, name, password } = body
+	try {
+		const body = await request.json();
+		const { email, name, password } = body;
 
-    if (!email || !name || !password) {
-      return new NextResponse("Missing info", { status: 400 })
-    }
+		if (!email || !name || !password) {
+			return new NextResponse("Missing info", { status: 400 });
+		}
 
-    const hashedPassword = await bcrypt.hash(password, 12)
+		const hashedPassword = await bcrypt.hash(password, 12);
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-      },
-    })
+		const user = await prisma.user.create({
+			data: {
+				email,
+				name,
+				hashedPassword,
+			},
+		});
 
-    return NextResponse.json(user)
-  } catch (error) {
-    console.log("Registration error", error)
-    return new NextResponse("Internal Error", { status: 500 })
-  }
+		return NextResponse.json(user);
+	} catch (error) {
+		console.log("Registration error", error);
+		return new NextResponse("Internal Error", { status: 500 });
+	}
 }

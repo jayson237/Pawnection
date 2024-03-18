6b148c81f0b350033ad7348d9fc401309365b4dc
Jayson Ng <jaysonng3@gmail.com>
2024-03-18 10:57:48 +0800

    feat: added hover on nav menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -33,35 +33,33 @@ function NavBar() {
         }}
       />
       <NavigationMenu>
-        <NavigationMenuList className="hidden sm:flex">
+        <NavigationMenuList className="hidden sm:flex gap-x-0.5">
           {session.status === "authenticated" && (
             <>
-              <div className="flex gap-10 items-center">
-                <Link
-                  href="/lost-and-found"
-                  className="text-primary text-sm font-medium"
-                >
-                  Lost & Found
-                </Link>
-                <Link
-                  href="/adopt"
-                  className="text-primary text-sm font-medium"
-                >
-                  Adopt
-                </Link>
-                <Link
-                  href="/commounity"
-                  className="text-primary text-sm font-medium"
-                >
-                  Community
-                </Link>
-                <Link
-                  href="/recommendations"
-                  className="text-primary text-sm font-medium"
-                >
-                  Recommendations
-                </Link>
-              </div>
+              <Link
+                href="/lost-and-found"
+                className="text-primary text-sm font-medium hover:bg-accent px-4 py-2 rounded-md ease-in-out duration-200"
+              >
+                Lost & Found
+              </Link>
+              <Link
+                href="/adopt"
+                className="text-primary text-sm font-medium hover:bg-accent px-4 py-2 rounded-md ease-in-out duration-200"
+              >
+                Adopt
+              </Link>
+              <Link
+                href="/commounity"
+                className="text-primary text-sm font-medium hover:bg-accent px-4 py-2 rounded-md ease-in-out duration-200"
+              >
+                Community
+              </Link>
+              <Link
+                href="/recommendations"
+                className="text-primary text-sm font-medium hover:bg-accent px-4 py-2 rounded-md ease-in-out duration-200"
+              >
+                Recommendations
+              </Link>
             </>
           )}
 

bb10c8725348f1570b805272905aa4cb16552b85
Jayson Ng <jaysonng3@gmail.com>
2024-03-23 21:56:27 +0800

    chore: ran run format

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,9 +1,9 @@
-import { Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react"
 
 export const Spinner = () => {
-	return (
-		<div className="flex flex-col align-middle h-full">
-			<Loader2 className="m-auto animate-spin" />
-		</div>
-	);
-};
+  return (
+    <div className="flex flex-col align-middle h-full">
+      <Loader2 className="m-auto animate-spin" />
+    </div>
+  )
+}

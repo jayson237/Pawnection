15e85c4b5a2f1274a3aec46a9aff68dbcceb8e34
Jayson Ng <jaysonng3@gmail.com>
2024-03-26 09:20:12 +0800

    feat: edit petcaresection

--- src/components/static/PetCareSection.tsx
+++ src/components/static/PetCareSection.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import Image from "next/image"
 import React, { useState } from "react"
 
@@ -15,7 +16,6 @@ const PetCareTips = () => {
   const [expandedTipId, setExpandedTipId] = useState<string | null>(null)
 
   const handleTipClick = (id: string) => {
-    // Toggle the expanded state for the clicked tip
     setExpandedTipId(expandedTipId === id ? null : id)
   }
 
@@ -23,6 +23,7 @@ const PetCareTips = () => {
     {
       id: "bonding",
       title: "Bonding",
+      message: "The secret to a stronger bond with your pet",
       description:
         "You can build a good relationship with your pets by rewarding them and walking them regularly!",
       icon: "/static/images/bonding.svg",
@@ -30,6 +31,7 @@ const PetCareTips = () => {
     {
       id: "dog-training",
       title: "Dog Training",
+      message: "The right way to train your dog",
       description:
         "Can be counter-effective if done wrongly. Don't hit your pets if you can't catch them on spot!",
       icon: "/static/images/dog-training.svg",
@@ -37,6 +39,7 @@ const PetCareTips = () => {
     {
       id: "pet-health",
       title: "Pet Health",
+      message: "How to drastically reduce your pet's risk of illness",
       description:
         "Remember to get your pets vax-ed cyclically. 70% of pet mortality is because of illness!",
       icon: "/static/images/pet-health.svg",
@@ -48,11 +51,13 @@ const PetCareTips = () => {
       <div className="py-[60px] bg-submain rounded-[50px]">
         <HeaderTitle className="max-w-full">Pet care tips</HeaderTitle>
 
-        <div className={`mt-6 flex gap-10 justify-center flex-wrap ${expandedTipId ? "items-start" : "items-center"}`}>
+        <div
+          className={`mt-6 flex gap-10 justify-center flex-wrap ${expandedTipId ? "items-start" : "items-center"}`}
+        >
           {tips.map((tip) => (
             <div
               key={tip.id}
-              className={`flex flex-col items-center rounded-[50px] px-10 pt-8 pb-5 ${expandedTipId ? (expandedTipId === tip.id ? "max-w-[580px]" : "hidden") : "max-w-[290px]"} bg-white transition-all duration-300 ease-in-out`}
+              className={`flex flex-col items-center rounded-[50px] px-10 pt-8 pb-5 ${expandedTipId ? (expandedTipId === tip.id ? "max-w-[580px]" : "hidden") : "max-w-[290px]"} bg-white transition-all duration-300 ease-in-out hover:cursor-pointer hover:shadow-lg hover:scale-105 hover:z-10`}
               onClick={() => handleTipClick(tip.id)}
             >
               <Image
@@ -63,7 +68,11 @@ const PetCareTips = () => {
               />
               <div className="space-y-6 text-center">
                 <h3 className="text-2xl font-semibold">{tip.title}</h3>
-                <p>{tip.description}</p>
+                {expandedTipId && expandedTipId === tip.id ? (
+                  <p>{tip.description}</p>
+                ) : (
+                  <p>{tip.message}</p>
+                )}
               </div>
             </div>
           ))}

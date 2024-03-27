696c9168552157e977223aa8a812b297a8e016ad
phonglm <e0960547@u.nus.edu>
2024-03-28 03:51:18 +0700

    Update: HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import WelcomeSection from "@/components/Home/WelcomeSection"
+import PetTipsAdvice from "@/components/Home/PetTipsAdvice"
+import TestimonialsSection from "@/components/Home/TestimonialsSection"
+import FeaturedPets from "@/components/Home/FeaturedPets"
+import LostAndFoundForm from "@/components/Home/LostAndFoundForm"
+import LatestPetStories from "@/components/Home/StoryCard"
+import PetSittingSection from "@/components/Home/PetSittingSection"
+import HomePetHangout from "@/components/Home/HomePetHangout"
+
 export default async function Home() {
   return (
-    <div className="h-full w-full flex flex-col place-content-center place-items-center">
-      Home
-    </div>
+    // <div className="h-full w-full flex flex-col place-content-center place-items-center">
+    //   Homessss
+    // </div>
+    // eslint-disable-next-line react/jsx-no-undef
+    <>
+      <WelcomeSection></WelcomeSection>
+      <PetTipsAdvice/>
+      <TestimonialsSection/>
+      <FeaturedPets/>
+      <LostAndFoundForm/>
+      <LatestPetStories/>
+      <PetSittingSection/>
+      <HomePetHangout/>
+    </>
   )
 }

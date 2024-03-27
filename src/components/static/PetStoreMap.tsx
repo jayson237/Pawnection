e68aa425712dac58639b04109724af9e37d2aa2e
justinlsl22 <justinlsl2201@gmail.com>
2024-03-27 21:20:39 +0800

    feat: page-rescaling

--- src/components/static/PetStoreMap.tsx
+++ src/components/static/PetStoreMap.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import "leaflet/dist/leaflet.css"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 import HeaderTitle from "../HeaderTitle"
@@ -13,6 +13,27 @@ const PetStoreMap = () => {
     // Add more pet store locations here
   ]
 
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+  console.log(windowDimensions)
+
+  const mapWidth = windowDimensions.width > 768 ? "1100px" : "75%"
+  console.log(mapWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
     <div className="space-y-6 flex flex-col items-center justify-center w-full max-w-[1240px] mx-auto md:px-0 px-4">
       <div className="py-[60px] space-y-6">
@@ -20,22 +41,25 @@ const PetStoreMap = () => {
           See them on our mini map!
         </HeaderTitle>
 
-        <MapContainer
-          center={[10.776889, 106.700806]}
-          zoom={13}
-          scrollWheelZoom={false}
-          style={{ height: "312px", width: "1100px" }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {petStores.map((store) => (
-            <Marker key={store.id} position={[store.lat, store.lng]}>
-              <Popup>{store.name}</Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+        <div className="flex justify-center w-full">
+          <MapContainer
+            center={[10.776889, 106.700806]}
+            zoom={13}
+            scrollWheelZoom={false}
+            // style={{ height: "312px", width: "1100px" }}
+            style={{ height: windowDimensions.height * 0.5, width: mapWidth }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {petStores.map((store) => (
+              <Marker key={store.id} position={[store.lat, store.lng]}>
+                <Popup>{store.name}</Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </div>
       </div>
     </div>
   )

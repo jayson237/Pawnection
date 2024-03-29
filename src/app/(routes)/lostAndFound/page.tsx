0dbb856c3cb874088d40d46b19bbf42d687f97e2
Jayson Ng <jaysonng3@gmail.com>
2024-03-29 13:00:38 +0800

    fix: fix build

--- src/app/(routes)/lostAndFound/page.tsx
+++ src/app/(routes)/lostAndFound/page.tsx
@@ -1,8 +1,6 @@
 import getAllFoundPetReports from "@/actions/getAllFoundPetReports"
 import getAllLostPetReports from "@/actions/getAllLostPetReports"
 import getCurrentUser from "@/actions/getCurrentUser"
-import PostForm from "@/components/community/posts/PostForm"
-import PostList from "@/components/community/posts/PostList"
 import LostAndFound from "@/components/lostAndFound/LostAndFound"
 import { redirect } from "next/navigation"
 

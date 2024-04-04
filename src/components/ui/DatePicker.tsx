0bf0b40a766feb372fd36a4aa0bb4875df0abefe
jayson237 <jaysonng3@gmail.com>
2024-04-04 10:53:55 +0800

    feat: dateTime picker

---
+++ src/components/ui/DatePicker.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import * as React from "react"
+
+import { Button } from "../ui/Button"
+import { Calendar } from "../ui/Calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/Popover"
+
+export function DatePickerDemo() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}

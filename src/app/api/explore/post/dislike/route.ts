abffef1a34dff8d8d99c758d63be84caa8ac4284
raymondtju <raymond.rtju@gmail.com>
2024-04-08 16:37:18 +0700

    feat: optimzed like/dis performace

--- src/app/api/explore/post/dislike/route.ts
+++ src/app/api/explore/post/dislike/route.ts
@@ -3,65 +3,58 @@ import prisma from "@/lib/prismadb"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
-  const currentUser = await getCurrentUser()
-  if (!currentUser) {
+  try {
+    const currentUser = await getCurrentUser()
+    if (!currentUser) {
+      return NextResponse.json(
+        {
+          message:
+            "You are not authorized to perform this action, please log in first",
+        },
+        { status: 401 },
+      )
+    }
+
+    const { postId }: { postId: string } = await req.json()
+    await prisma.$transaction(async (tx) => {
+      const deleteLike = await tx.like.delete({
+        where: {
+          userId_postId: {
+            userId: currentUser.id,
+            postId: postId,
+          },
+        },
+      })
+      if (!deleteLike) {
+        return NextResponse.json(
+          { message: "Unable to remove, please try again" },
+          { status: 400 },
+        )
+      }
+
+      const updateLikeCount = await tx.post.update({
+        data: {
+          likes_count: {
+            decrement: 1,
+          },
+        },
+        where: {
+          id: postId,
+        },
+      })
+      if (!updateLikeCount) {
+        return NextResponse.json(
+          { message: "Failed to update count" },
+          { status: 404 },
+        )
+      }
+    })
+
+    return NextResponse.json({ message: "Like removed" }, { status: 200 })
+  } catch (error) {
     return NextResponse.json(
-      {
-        message:
-          "You are not authorized to perform this action, please log in first",
-      },
-      { status: 401 },
+      { message: "Internal server error" },
+      { status: 500 },
     )
   }
-
-  const { postId }: { postId: string } = await req.json()
-  const getPost = await prisma.post.findUnique({
-    where: {
-      id: postId,
-    },
-  })
-
-  if (!getPost) {
-    return NextResponse.json({ message: "Post not found" }, { status: 400 })
-  }
-
-  const likeRecord = await prisma.like.findFirst({
-    where: {
-      postId: postId,
-      userId: currentUser.id,
-    },
-  })
-
-  if (!likeRecord) {
-    return NextResponse.json(
-      { message: "Like not found or could not be removed" },
-      { status: 404 },
-    )
-  }
-
-  const deleteLike = await prisma.like.delete({
-    where: {
-      id: likeRecord.id,
-    },
-  })
-
-  if (!deleteLike) {
-    return NextResponse.json(
-      { message: "Unable to remove, please try again" },
-      { status: 400 },
-    )
-  }
-
-  await prisma.post.update({
-    data: {
-      likes_count: {
-        decrement: 1,
-      },
-    },
-    where: {
-      id: postId,
-    },
-  })
-
-  return NextResponse.json({ message: "Like removed" }, { status: 200 })
 }
--- src/app/api/explore/post/like/route.ts
+++ src/app/api/explore/post/like/route.ts
@@ -3,56 +3,64 @@ import prisma from "@/lib/prismadb"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
-  const currentUser = await getCurrentUser()
-  if (!currentUser) {
-    return NextResponse.json(
-      {
-        message:
-          "You are not authorized to perform this action, please log in first",
-      },
-      { status: 401 },
-    )
-  }
+  try {
+    const currentUser = await getCurrentUser()
+    if (!currentUser) {
+      return NextResponse.json(
+        {
+          message:
+            "You are not authorized to perform this action, please log in first",
+        },
+        { status: 401 },
+      )
+    }
 
-  const { postId }: { postId: string } = await req.json()
-  const getPost = await prisma.post.findUnique({
-    where: {
-      id: postId,
-    },
-  })
+    const { postId }: { postId: string } = await req.json()
+    await prisma.$transaction(async (tx) => {
+      const likePost = await tx.like.create({
+        data: {
+          post: {
+            connect: {
+              id: postId,
+            },
+          },
+          user: {
+            connect: {
+              id: currentUser.id,
+            },
+          },
+        },
+      })
+      if (!likePost) {
+        return NextResponse.json(
+          { message: "Please try again" },
+          { status: 400 },
+        )
+      }
 
-  if (!getPost) {
-    return NextResponse.json({ message: "Post not found" }, { status: 404 })
-  }
-
-  const likePost = await prisma.like.create({
-    data: {
-      post: {
-        connect: {
+      const updateLikeCount = await tx.post.update({
+        data: {
+          likes_count: {
+            increment: 1,
+          },
+        },
+        where: {
           id: postId,
         },
-      },
-      user: {
-        connect: {
-          id: currentUser.id,
-        },
-      },
-    },
-  })
+      })
+      if (!updateLikeCount) {
+        return NextResponse.json(
+          { message: "Failed to update count" },
+          { status: 404 },
+        )
+      }
+    })
 
-  await prisma.post.update({
-    data: {
-      likes_count: {
-        increment: 1,
-      },
-    },
-    where: {
-      id: postId,
-    },
-  })
-
-  if (!likePost) {
-    return NextResponse.json({ message: "Please try again" }, { status: 400 })
+    return NextResponse.json({ message: "Post liked" }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 },
+    )
   }
-  return NextResponse.json({ message: "Post liked" }, { status: 200 })
 }

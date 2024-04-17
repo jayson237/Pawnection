979ca107037e04f957c78283d1e507f3bbfc9105
Jayson Ng <jaysonng@Jaysons-MacBook-Pro.local>
2024-04-17 16:29:53 +0800

    fix: fix onboarding with google

--- src/app/(routes)/auth/type/username/page.tsx
+++ src/app/(routes)/auth/type/username/page.tsx
@@ -1,14 +1,11 @@
 import UsernameForm from "@/components/auth/UsernameForm"
 import { getCurrentUser } from "@/lib/actions/user"
-import { UserType } from "@prisma/client"
 import { redirect } from "next/navigation"
 
 export default async function AuthUsername() {
   const currUser = await getCurrentUser()
   if (!currUser) redirect("/auth")
   if (currUser.username) redirect("/")
-  if (currUser.type === UserType.PetAdoptionCentre) redirect("/adoptionCenter")
-  else if (currUser.type === UserType.PetLover) redirect("/adoption")
 
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center pb-24">

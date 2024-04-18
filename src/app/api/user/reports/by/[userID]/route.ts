d63fcb6668c688bc4d21c71bce559817c8464653
raymondtju <raymond.rtju@gmail.com>
2024-04-18 16:34:04 +0700

    feat: change styling for profile reports child

---
+++ src/app/api/user/reports/by/[userID]/route.ts
@@ -0,0 +1,37 @@
+import { getCurrentUser } from "@/lib/actions/user"
+import prisma from "@/lib/prismadb"
+import { NextResponse } from "next/server"
+
+export async function GET(
+  request: Request,
+  {
+    params,
+  }: {
+    params: {
+      userID: string
+    }
+  },
+) {
+  const currentUser = await getCurrentUser()
+  try {
+    const userId = params.userID
+    const foundPetReports = await prisma.foundPetReport.findMany({
+      where: {
+        userId: userId,
+      },
+    })
+
+    const lostPetReports = await prisma.lostPetReport.findMany({
+      where: {
+        userId: userId,
+      },
+    })
+
+    const combinedReports = [...foundPetReports, ...lostPetReports]
+
+    return NextResponse.json(combinedReports)
+  } catch (error) {
+    console.log("Update error:", error)
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}

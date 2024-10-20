ed29ff20f067ca59f7e273fad7eff8cb8c8d87e3
jayson237 <jaysonng3@gmail.com>
2024-10-21 00:11:58 +0800

    feat: unoptimized next image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
